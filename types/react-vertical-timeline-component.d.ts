9081cc3c65eb5cc7e2745c2ede5f935f9a1300cd
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-02-22 19:30:15 +0530

    Fix ESLint config and add type declaration for react-vertical-timeline-component

---
+++ types/react-vertical-timeline-component.d.ts
@@ -0,0 +1,6 @@
+declare module "react-vertical-timeline-component" {
+    import { ComponentType } from "react";
+  
+    export const VerticalTimeline: ComponentType<any>;
+    export const VerticalTimelineElement: ComponentType<any>;
+  }
